a3e0403406a2f7a80308ab6c25e61d7f9d2786d1
Haris Zubair <haris@Haris-MacBook-Air.local>
2021-07-12 23:36:57 +0500

    removed forget password from top link

--- resources/views/layouts/dashboard/navbar.blade.php
+++ resources/views/layouts/dashboard/navbar.blade.php
@@ -1,20 +1,20 @@
 <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <!-- <li class="nav-item d-none d-sm-inline-block">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Left navbar links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <!-- <li class="nav-item d-none d-sm-inline-block">
         <a href="index3.html" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li> -->
-    </ul>
+  </ul>
 
-    <!-- SEARCH FORM -->
-    <!-- <form class="form-inline ml-3">
+  <!-- SEARCH FORM -->
+  <!-- <form class="form-inline ml-3">
       <div class="input-group input-group-sm">
         <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
         <div class="input-group-append">
@@ -24,37 +24,35 @@
         </div>
       </div>
     </form> -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Messages Dropdown Menu -->
-<!-- Notifications Dropdown Menu -->
-     
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <div class="btn-group">
-                    <button type="button" class="btn btn-default">{{Auth::user()->email}}</button>
-                    <button type="button" class="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
-                      <span class="sr-only">Toggle Dropdown</span>
-                    </button>
-                    <div class="dropdown-menu" role="menu">
-                      <a class="dropdown-item" href="{{URL('change_password')}}">Change password</a>
-                      <a class="dropdown-item"  href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+  <ul class="navbar-nav ml-auto">
+    <!-- Messages Dropdown Menu -->
+    <!-- Notifications Dropdown Menu -->
+
+    <li class="nav-item">
+      <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+        <i class="fas fa-expand-arrows-alt"></i>
+      </a>
+    </li>
+    <div class="btn-group">
+      <button type="button" class="btn btn-default">{{Auth::user()->email}}</button>
+      <button type="button" class="btn btn-default dropdown-toggle dropdown-icon" data-toggle="dropdown">
+        <span class="sr-only">Toggle Dropdown</span>
+      </button>
+      <div class="dropdown-menu" role="menu">
+        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}</i>
+          {{ __('Logout') }}</i>
 
-                                    </a>
+        </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none; ">
-                                        @csrf
-                                    </form>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none; ">
+          @csrf
+        </form>
 
-                    </div>
-                  </div>
-      </li>
+      </div>
+    </div>
+    </li>
     <!-- Right navbar links -->
-    </ul>
-  </nav>
-  <!-- /.navbar -->
+  </ul>
+</nav>
+<!-- /.navbar -->
