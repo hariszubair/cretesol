71046e3c42dfd70161aad08d1e82fddf7ee82382
Haris Zubair <haris@Haris-MacBook-Air.local>
2021-07-13 22:04:20 +0500

    google analytics

--- resources/views/layouts/dashboard/header.blade.php
+++ resources/views/layouts/dashboard/header.blade.php
@@ -4,6 +4,7 @@ This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,9 +19,24 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="{{asset('public/css/adminlte.min.css')}}">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{asset('public/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-   <!-- Tempusdominus Bootstrap 4 -->
+  <!-- Tempusdominus Bootstrap 4 -->
   <link rel="stylesheet" href="{{asset('public/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-77921144-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-77921144-1');
+  </script>
+
+
 </head>
+
 <body class="hold-transition sidebar-mini">
-	@include('sweet::alert')
-<div class="wrapper">
+  @include('sweet::alert')
+  <div class="wrapper">
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,97 +2,121 @@
 <html lang="en">
 
 <head>
-<meta name="csrf-token" content="{{ csrf_token() }}">
-     <meta name="description" content="Cretesol is a Stone & Tile Company based in Pakistan, Specialist in Imported Marble, Granite, Quartzite, Travertine & Ceramic, Porcelain Tiles. With the presence in the city of Islamabad(Flagship), Lahore and Karachi.">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="description" content="Cretesol is a Stone & Tile Company based in Pakistan, Specialist in Imported Marble, Granite, Quartzite, Travertine & Ceramic, Porcelain Tiles. With the presence in the city of Islamabad(Flagship), Lahore and Karachi.">
   <meta name="keywords" content="Cretesol, title, stone,cretesol.com,marble">
   <meta name="author" content="Cretesol">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <title>Cretesol | Tiles  Stones</title>
-    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/royal-preload.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/flaticon.css')}}" rel="stylesheet">
-    <link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet">
-    <!-- <link href="{{asset('css/owl.theme.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset('css/lightgallery.css')}}" rel="stylesheet"> -->
-    <!-- <link href="{{asset('css/woocommerce.css')}}" rel="stylesheet"> -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
-    <!-- REVOLUTION SLIDER CSS -->
-    <link href="{{asset('plugins/revolution/revolution/css/settings.css')}}" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+  <title>Cretesol | Tiles Stones</title>
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/royal-preload.css')}}" rel="stylesheet">
+  <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/flaticon.css')}}" rel="stylesheet">
+  <link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet">
+  <!-- <link href="{{asset('css/owl.theme.css')}}" rel="stylesheet"> -->
+  <!-- <link href="{{asset('css/magnific-popup.css')}}" rel="stylesheet"> -->
+  <!-- <link href="{{asset('css/lightgallery.css')}}" rel="stylesheet"> -->
+  <!-- <link href="{{asset('css/woocommerce.css')}}" rel="stylesheet"> -->
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <!-- REVOLUTION SLIDER CSS -->
+  <link href="{{asset('plugins/revolution/revolution/css/settings.css')}}" rel="stylesheet">
 
-    <!-- REVOLUTION NAVIGATION STYLE -->
-    <link href="{{asset('plugins/revolution/revolution/css/navigation.css')}}" rel="stylesheet">
+  <!-- REVOLUTION NAVIGATION STYLE -->
+  <link href="{{asset('plugins/revolution/revolution/css/navigation.css')}}" rel="stylesheet">
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-77921144-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-77921144-1');
+  </script>
+
+
 </head>
 <style type="text/css">
-    .cate-lines .cate-item_content {
-   
-     left: 0; 
-}
-.icon-box--is-line-hover:hover {
--webkit-filter: brightness(80%) !important;
+  .cate-lines .cate-item_content {
 
-}
-.icon-box--is-line-hover:hover div h2{
-color:grey !important;
-}
-.is-stuck {
-    opacity: 90%;
-  background-color: black;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 99;
-  border: none !important;
-  -webkit-animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
-  -moz-animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
-  animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
-}
-@media (min-width: 1024px) {
-  #main_logo {
-    margin-left: 95px;
+    left: 0;
   }
-  
-}
-@font-face {
-  font-family: 'navBarFont';
-  src: 
-        url("{{asset('fonts/A.otf')}}");
-        format('truetype');
-  font-style: normal;
-  font-weight: 100;
-}
-@font-face {
-  font-family: 'sliderFont';
-  src: 
-        url("{{asset('fonts/B.ttf')}}");
-        format('truetype');
-  font-style: normal;
-  font-weight: 100;
-}
-.navBarFont{
-    font-family:'navBarFont'
-}
-.sliderFont{
-    font-family:'sliderFont'
-}
-.aFont{
-    font-family:'navBarFont'
-} 
-.bFont{
-    font-family:'sliderFont'
-}
-#tawkchat-chat-bubble-close{
-display: none !important;
-}
-.theme-background-color{
-  background-color: #2a2828 !important;
-}
+
+  .icon-box--is-line-hover:hover {
+    -webkit-filter: brightness(80%) !important;
+
+  }
+
+  .icon-box--is-line-hover:hover div h2 {
+    color: grey !important;
+  }
+
+  .is-stuck {
+    opacity: 90%;
+    background-color: black;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 99;
+    border: none !important;
+    -webkit-animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
+    -moz-animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
+    animation: stickySlideDown 0.65s cubic-bezier(0.23, 1, 0.32, 1) both;
+  }
+
+  @media (min-width: 1024px) {
+    #main_logo {
+      margin-left: 95px;
+    }
+
+  }
+
+  @font-face {
+    font-family: 'navBarFont';
+    src: url("{{asset('fonts/A.otf')}}");
+    format('truetype');
+    font-style: normal;
+    font-weight: 100;
+  }
+
+  @font-face {
+    font-family: 'sliderFont';
+    src: url("{{asset('fonts/B.ttf')}}");
+    format('truetype');
+    font-style: normal;
+    font-weight: 100;
+  }
+
+  .navBarFont {
+    font-family: 'navBarFont'
+  }
+
+  .sliderFont {
+    font-family: 'sliderFont'
+  }
+
+  .aFont {
+    font-family: 'navBarFont'
+  }
+
+  .bFont {
+    font-family: 'sliderFont'
+  }
+
+  #tawkchat-chat-bubble-close {
+    display: none !important;
+  }
+
+  .theme-background-color {
+    background-color: #2a2828 !important;
+  }
 </style>
+
 <body>
-  
-    <div id="royal_preloader"></div>
-    
+
+  <div id="royal_preloader"></div>
